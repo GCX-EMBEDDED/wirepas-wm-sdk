862b88006db14cddc442fa2f7d6bc1132d90a56b
Tareq-Mh <tareq.mhisen@grandcentrix.net>
2023-09-08 14:19:39 +0200

    gcx_apps: Use the hts221 sensor and send its measurements

--- source/gcx_apps/Low_power_room_monitoring_app/app.c
+++ source/gcx_apps/Low_power_room_monitoring_app/app.c
@@ -15,6 +15,10 @@
 #include "shared_data.h"
 #include "app_scheduler.h"
 
+#include "board.h"
+#include "hts221.h"
+#include "gpio.h"
+
 #define DEBUG_LOG_MODULE_NAME "MEETING_ROOM_MONITOR_APP"
 /** To activate logs, configure the following line with "LVL_INFO". */
 #define DEBUG_LOG_MAX_LEVEL LVL_DEBUG
@@ -33,6 +37,8 @@
 
 #define DATA_EP 1
 
+#define MSG_ID_READING 0
+
 /** Period to send measurements, in ms */
 static uint32_t period_ms;
 
@@ -42,6 +48,17 @@ typedef struct __attribute__((packed))
     uint32_t period_ms;
 } payload_periodic_t;
 
+struct env_data
+{
+    bool temp_to_send;
+    int8_t temp_integer;
+    uint8_t temp_decimal;
+    bool humi_to_send;
+    int16_t humidity;
+};
+
+static struct env_data environmental_data;
+
 /**
  * @brief   Task to send periodic message.
  * @return  next period
@@ -50,15 +67,23 @@ typedef struct __attribute__((packed))
 static uint32_t send_data_task(void)
 {
     static uint32_t id = 0; // Value to send
+    static uint8_t buffer[6];
+
+    buffer[0] = MSG_ID_READING;
+    buffer[1] = id;
+    buffer[2] = environmental_data.temp_integer;
+    buffer[3] = environmental_data.temp_decimal;
+    buffer[4] = environmental_data.humidity;
+    buffer[5] = (environmental_data.humidity >> 8);
 
     // Create a data packet to send
     app_lib_data_to_send_t data_to_send;
-    data_to_send.bytes = (const uint8_t *)&id;
-    data_to_send.num_bytes = sizeof(id);
+    data_to_send.bytes = (const uint8_t *)buffer;
+    data_to_send.num_bytes = sizeof(buffer);
     data_to_send.dest_address = APP_ADDR_ANYSINK;
     data_to_send.src_endpoint = DATA_EP;
     data_to_send.dest_endpoint = DATA_EP;
-    data_to_send.qos = APP_LIB_DATA_QOS_HIGH;
+    data_to_send.qos = APP_LIB_DATA_QOS_NORMAL;
     data_to_send.delay = 0;
     data_to_send.flags = APP_LIB_DATA_SEND_FLAG_NONE;
     data_to_send.tracking_id = APP_LIB_DATA_NO_TRACKING_ID;
@@ -69,10 +94,45 @@ static uint32_t send_data_task(void)
     // Increment value to send
     id++;
 
+    environmental_data.temp_to_send = false;
+    environmental_data.humi_to_send = false;
     // Inform the stack that this function should be called again in
     // period_ms microseconds. By returning APP_SCHEDULER_STOP_TASK,
     // the stack won't call this function again.
     return period_ms;
+}
+
+/**
+ * @brief   Task to send periodic message.
+ * @return  next period
+ */
+
+static uint32_t trigger_sensors_task(void)
+{
+    hts221_one_shot();
+    return 10000;
+}
+
+void hts221_interrupt_handler(uint8_t pin, gpio_event_e event)
+{
+    float f_decimal;
+    float in_temp = hts221_temperature_get();
+    environmental_data.temp_integer = (int8_t)in_temp;
+    f_decimal = in_temp - environmental_data.temp_integer;
+    environmental_data.temp_decimal = (uint8_t)(f_decimal * 100.0f);
+    LOG(LVL_DEBUG, "Temperature: %d.%d C", environmental_data.temp_integer, environmental_data.temp_decimal);
+    int16_t humidity = hts221_humidity_get();
+    LOG(LVL_DEBUG, "Relative Humidty: %d%%", humidity);
+    environmental_data.temp_to_send = true;
+    environmental_data.humi_to_send = true;
+}
+
+static uint32_t init_sensors(void)
+{
+    hts221_init();
+    hts221_enable();
+    GPIO_register_for_event(24, GPIO_NOPULL, GPIO_EVENT_HL, 100, hts221_interrupt_handler);
+    return APP_SCHEDULER_STOP_TASK;
 }
 
 /**
@@ -86,6 +146,7 @@ void App_init(const app_global_functions_t *functions)
 {
     LOG_INIT();
     LOG(LVL_INFO, "App_init");
+
     // Basic configuration of the node with a unique node address
     if (configureNodeFromBuildParameters() != APP_RES_OK)
     {
@@ -107,6 +168,17 @@ void App_init(const app_global_functions_t *functions)
     App_Scheduler_addTask_execTime(send_data_task,
                                    APP_SCHEDULER_SCHEDULE_ASAP,
                                    EXECUTION_TIME_US);
+
+    App_Scheduler_addTask_execTime(trigger_sensors_task,
+                                   APP_SCHEDULER_SCHEDULE_ASAP,
+                                   EXECUTION_TIME_US);
+
+    // Enable power for sensors
+    nrf_gpio_pin_dir_set(VDD_PWD_CTRL, NRF_GPIO_PIN_DIR_OUTPUT);
+    nrf_gpio_cfg_output(VDD_PWD_CTRL);
+    nrf_gpio_pin_set(VDD_PWD_CTRL); // consumes about 70 µA
+
+    App_Scheduler_addTask_execTime(init_sensors, 1000, 50 * 1000);
 
     // Start the stack
     lib_state->startStack();
