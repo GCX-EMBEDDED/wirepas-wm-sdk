327167975d6b53426b8255e68391ca71ac8ac4a9
Tareq-Mh <tareq.mhisen@grandcentrix.net>
2023-09-12 15:36:52 +0200

    gcx_apps: Imrove the logic of the app and clean it

--- source/gcx_apps/Low_power_room_monitoring_app/app.c
+++ source/gcx_apps/Low_power_room_monitoring_app/app.c
@@ -1,11 +1,11 @@
-/* Copyright 2017 Wirepas Ltd. All Rights Reserved.
+/* Copyright (c) 2023 grandcentrix GmbH
  *
- * See file LICENSE.txt for full license details.
+ * SPDX-License-Identifier: MIT
  */
 
 /*
  * \file    app.c
- * \brief   This file is a template for writing a custom application
+ * \brief   This file is a template for writing a Meeting room monitor application
  */
 
 #include <stdlib.h>
@@ -21,68 +21,55 @@
 #include "battery_measurement.h"
 #include "sx1509.h"
 
-#define REG_DIR_B 0x0F  // Pin direction register - bank B
-#define REG_DIR_A 0x0E  // Pin direction register - bank A
-#define REG_DATA_B 0x11 // Data register - bank B
-#define REG_DATA_A 0x10 // Data register - bank A
-// SX1509 Pins
-#define BANK_A 0
-#define BANK_B 1
-
 #define DEBUG_LOG_MODULE_NAME "MEETING_ROOM_MONITOR_APP"
-/** To activate logs, configure the following line with "LVL_INFO". */
+
 #define DEBUG_LOG_MAX_LEVEL LVL_DEBUG
 
 #include "debug_log.h"
 
-/** Period to send data */
-#define DEFAULT_SEND_PERIOD_S 60
-#define DEFAULT_SEND_PERIOD_MS (DEFAULT_SEND_PERIOD_S * 1000)
-
 /** Period to trigger sensors */
-#define DEFAULT_MEASURE_PERIOD_S 50
+#define DEFAULT_MEASURE_PERIOD_S 60
 #define DEFAULT_MEASURE_PERIOD_MS (DEFAULT_MEASURE_PERIOD_S * 1000)
 
 /** Max time needed to execute the periodic work, in us */
 #define EXECUTION_TIME_US 2500
 
+/** Delay time needed before executing some tasks, in ms */
 #define TASK_DELAY_TIME_MS 1000
 
-/** Endpoint to change the sending period value */
-#define SET_PERIOD_EP 10
-
+/** Data end-point */
 #define DATA_EP 1
 
 #define MSG_ID_READING 0
 
-/** Period to send measurements, in ms */
-static uint32_t send_period_ms;
-static uint32_t measure_period_ms;
+enum measurement_state
+{
+    SM_MEAS_TRIGGER_TEMPERATURE_CONV,
+    SM_MEAS_WAIT_FOR_TEMPERATURE_CONV,
+    SM_MEAS_WAIT_FOR_GAS_SENSOR_CONV,
+};
+
+static enum measurement_state state;
 
 struct env_data
 {
-    bool temp_to_send;
     int8_t temp_integer;
     uint8_t temp_decimal;
-    bool humi_to_send;
     int16_t humidity;
     uint8_t movement_counter;
-    bool mov_count_to_send;
     uint16_t voltage;
-    bool volt_to_send;
 };
 
 static struct env_data environmental_data;
 
 /**
  * @brief   Task to send periodic message.
- * @return  next period
+ * @return  Next period
  */
-
 static uint32_t send_data_task(void)
 {
     static uint32_t id = 0; // Value to send
-    static uint8_t buffer[9];
+    static uint8_t buffer[11];
 
     buffer[0] = MSG_ID_READING;
     buffer[1] = id;
@@ -90,9 +77,11 @@ static uint32_t send_data_task(void)
     buffer[3] = (environmental_data.voltage >> 8);
     buffer[4] = environmental_data.temp_integer;
     buffer[5] = environmental_data.temp_decimal;
-    buffer[6] = environmental_data.humidity;
-    buffer[7] = (environmental_data.humidity >> 8);
-    buffer[8] = environmental_data.movement_counter;
+    buffer[6] = 255;        // Const value for now
+    buffer[7] = (100 >> 8); // Const value for now
+    buffer[8] = environmental_data.humidity;
+    buffer[9] = (environmental_data.humidity >> 8);
+    buffer[10] = environmental_data.movement_counter;
 
     // Create a data packet to send
     app_lib_data_to_send_t data_to_send;
@@ -111,32 +100,15 @@ static uint32_t send_data_task(void)
 
     // Increment value to send
     id++;
+    // Clear motion sensor count
+    environmental_data.movement_counter = 0;
 
-    environmental_data.temp_to_send = false;
-    environmental_data.humi_to_send = false;
-    environmental_data.mov_count_to_send = false;
-    environmental_data.volt_to_send = false;
-    // Inform the stack that this function should be called again in
-    // send_period_ms microseconds. By returning APP_SCHEDULER_STOP_TASK,
-    // the stack won't call this function again.
-    return send_period_ms;
+    LOG(LVL_DEBUG, "Message has been sent!");
+
+    return APP_SCHEDULER_STOP_TASK;
 }
 
-/**
- * @brief   Task to send periodic message.
- * @return  next period
- */
-
-static uint32_t trigger_sensors_task(void)
-{
-    hts221_one_shot();
-    environmental_data.voltage = battery_measurement_get();
-    LOG(LVL_DEBUG, "Battery voltage %lu mV", environmental_data.voltage);
-    environmental_data.volt_to_send = true;
-    return measure_period_ms;
-}
-
-void hts221_interrupt_handler(uint8_t pin, gpio_event_e event)
+static void read_temperature_and_humidity(void)
 {
     float f_decimal;
     float in_temp = hts221_temperature_get();
@@ -144,16 +116,76 @@ void hts221_interrupt_handler(uint8_t pin, gpio_event_e event)
     f_decimal = in_temp - environmental_data.temp_integer;
     environmental_data.temp_decimal = (uint8_t)(f_decimal * 100.0f);
     LOG(LVL_DEBUG, "Temperature: %d.%d C", environmental_data.temp_integer, environmental_data.temp_decimal);
-    int16_t humidity = hts221_humidity_get();
-    LOG(LVL_DEBUG, "Relative Humidty: %d%%", humidity);
-    environmental_data.temp_to_send = true;
-    environmental_data.humi_to_send = true;
+    environmental_data.humidity = hts221_humidity_get();
+    LOG(LVL_DEBUG, "Relative Humidty: %d%%", environmental_data.humidity);
+}
+
+static uint32_t measurement_sm_task(void)
+{
+    uint32_t next_period = APP_SCHEDULER_STOP_TASK;
+
+    switch (state)
+    {
+    case SM_MEAS_TRIGGER_TEMPERATURE_CONV:
+        // Trigger temperature and humidity sensor conversion
+        state = SM_MEAS_WAIT_FOR_TEMPERATURE_CONV;
+        hts221_one_shot();
+        break;
+
+    case SM_MEAS_WAIT_FOR_TEMPERATURE_CONV:
+        // Read temperature and humidity sensor
+        read_temperature_and_humidity();
+        // Write temperature/humidity to gas sensor
+
+        // Trigger gas sensor
+
+        state = SM_MEAS_WAIT_FOR_GAS_SENSOR_CONV;
+        // -> in gas sensor interrupt handler, schedule this task again
+
+        // Because the interrupt handler is not implemented yet, reschedule the task here again
+        App_Scheduler_addTask_execTime(measurement_sm_task,
+                                       APP_SCHEDULER_SCHEDULE_ASAP,
+                                       EXECUTION_TIME_US);
+        break;
+
+    case SM_MEAS_WAIT_FOR_GAS_SENSOR_CONV:
+        // Read gas sensor
+
+        // Read battery voltage
+        environmental_data.voltage = battery_measurement_get();
+        LOG(LVL_DEBUG, "Battery voltage %lu mV", environmental_data.voltage);
+
+        // Send data
+        App_Scheduler_addTask_execTime(send_data_task, APP_SCHEDULER_SCHEDULE_ASAP, EXECUTION_TIME_US);
+        state = SM_MEAS_TRIGGER_TEMPERATURE_CONV;
+
+        // Schedule this task in DEFAULT_MEASURE_PERIOD_MS again
+        App_Scheduler_addTask_execTime(measurement_sm_task,
+                                       DEFAULT_MEASURE_PERIOD_MS,
+                                       EXECUTION_TIME_US);
+        break;
+    default:
+        LOG(LVL_ERROR, "Unknown state %d", state);
+        break;
+    }
+
+    return next_period;
+}
+
+void hts221_interrupt_handler(uint8_t pin, gpio_event_e event)
+{
+    if (state == SM_MEAS_WAIT_FOR_TEMPERATURE_CONV)
+    {
+        App_Scheduler_addTask_execTime(measurement_sm_task,
+                                       APP_SCHEDULER_SCHEDULE_ASAP,
+                                       EXECUTION_TIME_US);
+    }
 }
 
 void hw416_interrupt_handler(uint8_t pin, gpio_event_e event)
 {
-    LOG(LVL_DEBUG, "Movement has been detected!, Movement_counter=%u", ++environmental_data.movement_counter);
-    environmental_data.mov_count_to_send = true;
+    ++environmental_data.movement_counter;
+    LOG(LVL_DEBUG, "Movement has been detected!, Movement_counter=%u", environmental_data.movement_counter);
 }
 
 static uint32_t init_sensors(void)
@@ -163,6 +195,13 @@ static uint32_t init_sensors(void)
     GPIO_register_for_event(24, GPIO_NOPULL, GPIO_EVENT_HL, 10, hts221_interrupt_handler);
     GPIO_register_for_event(4, GPIO_NOPULL, GPIO_EVENT_LH, 10, hw416_interrupt_handler);
     battery_measurement_init();
+    // Read the hts221 sensor to clear the flag of data-ready and to activate the interrupt handler by the next one-shot measurement
+    read_temperature_and_humidity();
+    state = SM_MEAS_TRIGGER_TEMPERATURE_CONV;
+    App_Scheduler_addTask_execTime(measurement_sm_task,
+                                   APP_SCHEDULER_SCHEDULE_ASAP,
+                                   EXECUTION_TIME_US);
+
     return APP_SCHEDULER_STOP_TASK;
 }
 
@@ -176,7 +215,7 @@ static uint32_t init_sensors(void)
 void App_init(const app_global_functions_t *functions)
 {
     LOG_INIT();
-    LOG(LVL_INFO, "App_init");
+    LOG(LVL_INFO, "MEETING_ROOM_MONITOR_APP INIT");
 
     // Basic configuration of the node with a unique node address
     if (configureNodeFromBuildParameters() != APP_RES_OK)
@@ -197,20 +236,10 @@ void App_init(const app_global_functions_t *functions)
     // Enable power for sensors
     nrf_gpio_pin_dir_set(VDD_PWD_CTRL, NRF_GPIO_PIN_DIR_OUTPUT);
     nrf_gpio_cfg_output(VDD_PWD_CTRL);
-    nrf_gpio_pin_set(VDD_PWD_CTRL); // consumes about 70 µA
+    nrf_gpio_pin_set(VDD_PWD_CTRL); // Consumes about 70 µA
 
     App_Scheduler_addTask_execTime(init_sensors, TASK_DELAY_TIME_MS, EXECUTION_TIME_US);
 
-    measure_period_ms = DEFAULT_MEASURE_PERIOD_MS;
-    App_Scheduler_addTask_execTime(trigger_sensors_task,
-                                   TASK_DELAY_TIME_MS,
-                                   EXECUTION_TIME_US);
-
-    // Set a periodic callback to be called after DEFAULT_SEND_PERIOD_MS
-    send_period_ms = DEFAULT_SEND_PERIOD_MS;
-    App_Scheduler_addTask_execTime(send_data_task,
-                                   APP_SCHEDULER_SCHEDULE_ASAP,
-                                   EXECUTION_TIME_US);
     // Start the stack
     lib_state->startStack();
 }
